8963ed294c3f21258a283c261c9e8f34e9fa95c8
Isabella Borges <isabellab.ferreira5@gmail.com>
2022-06-08 00:05:29 -0300

    feat: finalizando template do front

--- pets/src/pages/index.tsx
+++ pets/src/pages/index.tsx
@@ -13,7 +13,23 @@ const Home: NextPage = () => {
       <span>Com um pequeno valor mensal você <br />
       pode <strong>adotar um pet virtualmente</strong></span>} />
 
-      <Lista />
+      <Lista 
+        pets={[
+          {
+            id: 1,
+            nome: 'Bidu',
+            historia: 'alsdkjlasdjlasd alksdjalsda asdlkjadlajd aldjaldjasld',
+            foto: 'https://www.maxtotalalimentos.com.br/img/seo/9/377/5-coisas-que-fazem-seu-cachorro-viralata-feliz.jpg'
+          },
+          {
+            id: 2,
+            nome: 'Mezeno',
+            historia: 'alsdkjlasdjlasd alksdjalsda asdlkjadlajd aldjaldjasld',
+            foto: 'https://i.pinimg.com/564x/37/7c/b4/377cb4b88e459ab85e38bfea0d3631c3.jpg'
+          },
+
+        ]}
+      />
     </div>
   )
 }
--- pets/src/ui/components/Lista/Lista.tsx
+++ pets/src/ui/components/Lista/Lista.tsx
@@ -7,34 +7,34 @@ import {
     Nome,
     Descricao
 } from './Lista.Style'
+import { Pet } from '../../../data/@types/Pet'
+import { TextService } from '../../../data/services/TextService'
 
-export default function Lista() {
+interface ListaProps{
+    pets: Pet[];
+}
+
+export default function Lista(props: ListaProps) {
+    const tamanhoMaximoTexto = 200;
+    
     return (
         <ListaStyled>
-            <ItemLista>
-                <Foto src={'https://www.maxtotalalimentos.com.br/img/seo/9/377/5-coisas-que-fazem-seu-cachorro-viralata-feliz.jpg'}/>
-                <Informacoes>
-                    <Nome>Bidu</Nome>
-                    <Descricao>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cum culpa ad ipsum ex, sint iure tempora incidunt quas deleniti provident vero perferendis in libero earum necessitatibus, laborum dolores quam!
-                    </Descricao>
-                    <Button
-                        variant={'contained'}
-                    >Adotar</Button>
-                </Informacoes>
-            </ItemLista>
-            <ItemLista>
-                <Foto src={'https://www.maxtotalalimentos.com.br/img/seo/9/377/5-coisas-que-fazem-seu-cachorro-viralata-feliz.jpg'}/>
-                <Informacoes>
-                    <Nome>Bidu</Nome>
-                    <Descricao>
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius cum culpa ad ipsum ex, sint iure tempora incidunt quas deleniti provident vero perferendis in libero earum necessitatibus, laborum dolores quam!
-                    </Descricao>
-                    <Button
-                        variant={'contained'}
-                    >Adotar</Button>
-                </Informacoes>
-            </ItemLista>
+            {props.pets.map(pet => (
+                <ItemLista key={pet.id} >
+                    <Foto src={pet.foto} alt={pet.nome} />
+                    <Informacoes>
+                        <Nome>{pet.nome}</Nome>
+                        <Descricao>
+                            {TextService.limitarTexto(pet.historia, tamanhoMaximoTexto)}
+                        </Descricao>
+                        <Button
+                            variant={'contained'}
+                            fullWidth
+                        >Adotar {pet.nome}
+                        </Button>
+                    </Informacoes>
+                </ItemLista>
+            ))}
         </ListaStyled>
     )
 }
