a576d80a4a61bcd96683c58aff19ecf92076b51e
Isabella Borges <isabellab.ferreira5@gmail.com>
2022-06-09 17:34:32 -0300

    feat: começando a criar a lógica da aplicação de adoção

--- adote-um-pet/adote_um_pet/settings.py
+++ adote-um-pet/adote_um_pet/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pet.apps.PetConfig',
+    'adocao.apps.AdocaoConfig',
+    'rest_framework'
+    
 ]
 
 MIDDLEWARE = [
--- adote-um-pet/pet/views.py
+++ adote-um-pet/pet/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
-# Create your views here.
+from .models import Pet
+from .serializers import PetSerializer
+
+
+class PetList(APIView):
+    def get(self, request, formart=None):
+        pets = Pet.objects.all()
+        serializer = PetSerializer(pets, many=True)
+        return Response(serializer.data, status=HTTP_200_OK)
+
+
+
+
