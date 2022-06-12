fd9f51918ae90f45a0bcd33ea19225cd762ec77a
Isabella Borges <isabellab.ferreira5@gmail.com>
2022-06-11 21:59:36 -0300

    feat: finalizando projeto

--- pets/src/ui/components/Title/Title.style.tsx
+++ pets/src/ui/components/Title/Title.style.tsx
@@ -3,7 +3,7 @@ import { styled } from '@mui/material';
 export const TitleStyled = styled('h1')`
     font-style: 20px;
     text-align: center;
-    margin-bottom: ${({ theme }) => theme.spacing(5) }; 
+    margin-bottom: ${({ theme }) => theme.spacing(3) }; 
 `;
 
 export const Subtitle = styled('h2')`
